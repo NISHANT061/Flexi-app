684ec19a2717cb7f25be59bda2800d575a5463bf
Nishant Kumar <cmritnishant@gmail.com>
2020-08-30 00:42:35 +0530

    added connector css

--- src/App.js
+++ src/App.js
@@ -7,42 +7,43 @@ function App() {
     items: [
       {
         name: "person_name",
-
         label: "Person's Name",
-
         type: "TextField",
       },
 
       {
         name: "states",
-
         label: "Person's state",
-
         type: "DropDown",
-
         values: ["Maharashtra", "Kerala", "Tamil Nadu"],
       },
     ],
   };
-  const onSubmitHandler=(data)=>{
-    alert(`Data Recieved: Person's Name -> ${data.person_name},Person's state ->${data.state}`)
-  }
-  return <div className="App">
-    <div className="App-title"><h1>Flexi Tree</h1></div>
-    <div className="flexi-tree">
-    <Flexi config={flexiConfig} onSubmitHandler={onSubmitHandler}>
-    <Flexi config={flexiConfig} onSubmitHandler={onSubmitHandler}>
-    <Flexi config={flexiConfig} onSubmitHandler={onSubmitHandler}/>
-    <Flexi config={flexiConfig} onSubmitHandler={onSubmitHandler}>
-    <Flexi config={flexiConfig} onSubmitHandler={onSubmitHandler}/>
-    <Flexi config={flexiConfig} onSubmitHandler={onSubmitHandler}/>
-      </Flexi>
-      </Flexi>
-    <Flexi config={flexiConfig} onSubmitHandler={onSubmitHandler}/>
-    <Flexi config={flexiConfig} onSubmitHandler={onSubmitHandler}/>
-      </Flexi>
+  const onSubmitHandler = (data) => {
+    alert(
+      `Data Recieved: Person's Name -> ${data.person_name},Person's state ->${data.state}`
+    );
+  };
+  return (
+    <div className="App">
+      <div className="App-title">
+        <h1>Flexi Tree</h1>
       </div>
-  </div>;
+      <div className="flexi-tree">
+        <Flexi config={flexiConfig} onSubmitHandler={onSubmitHandler}>
+          <Flexi config={flexiConfig} onSubmitHandler={onSubmitHandler}>
+            <Flexi config={flexiConfig} onSubmitHandler={onSubmitHandler} />
+            <Flexi config={flexiConfig} onSubmitHandler={onSubmitHandler}>
+              <Flexi config={flexiConfig} onSubmitHandler={onSubmitHandler} />
+              <Flexi config={flexiConfig} onSubmitHandler={onSubmitHandler} />
+            </Flexi>
+          </Flexi>
+          <Flexi config={flexiConfig} onSubmitHandler={onSubmitHandler} />
+          <Flexi config={flexiConfig} onSubmitHandler={onSubmitHandler} />
+        </Flexi>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/Components/Flexi/Flexi.js
+++ src/Components/Flexi/Flexi.js
@@ -1,9 +1,23 @@
-import React, { useState } from "react";
-import "./Flexi.scss";
 import PropTypes from "prop-types";
-import {IdGenerator} from "../../utils/generateId"
+import React, { useLayoutEffect, useState, useRef } from "react";
+import { IdGenerator } from "../../utils/generateId";
+import "./Flexi.scss";
 
-const Flexi = ({ config, onSubmitHandler, children }) => {
+function getVlineHeight(ref, count) {
+  const dom = ref.current;
+  let height = 0;
+  if (dom) {
+    const domDimensions = dom.getBoundingClientRect();
+    const lastChildDimensions = dom.lastChild.getBoundingClientRect();
+    console.log(domDimensions.height, lastChildDimensions.height / 2);
+    height = domDimensions.height - lastChildDimensions.height;
+  }
+  return height;
+}
+
+const Flexi = ({ config, onSubmitHandler, children, isNested }) => {
+  const childrenRef = useRef(null);
+  const [loading, setLoading] = useState(true);
   const [data, setData] = useState({ person_name: "", state: "" });
   const childrenArray = React.Children.toArray(children);
   const editDataHandler = (field, value) => {
@@ -11,54 +25,75 @@ const Flexi = ({ config, onSubmitHandler, children }) => {
     tempData[field] = value;
     setData(tempData);
   };
+  useLayoutEffect(() => {
+    setLoading(false);
+  }, []);
   return (
     <ul>
       <li>
-        {config.items.map((item) =>
-          item.type === "TextField" ? (
-            <div key={`hcc-${IdGenerator()}`}>
-              <div className="label">{item.label}</div>
-              <div>
-                <input
-                  className="input"
-                  name={item.name}
-                  type="text"
-                  value={data.person_name}
-                  placeholder="Please enter name"
-                  onChange={(event) => {
-                    
-                    editDataHandler(item.name, event.target.value);
-                  }}
-                />
+        <div className="flexi-card">
+          {config.items.map((item) =>
+            item.type === "TextField" ? (
+              <div key={`hcc-${IdGenerator()}`}>
+                <div className="label">{item.label}</div>
+                <div>
+                  <input
+                    className="input"
+                    name={item.name}
+                    type="text"
+                    value={data.person_name}
+                    placeholder="Please enter name"
+                    onChange={(event) => {
+                      editDataHandler(item.name, event.target.value);
+                    }}
+                  />
+                </div>
               </div>
-            </div>
-          ) : item.type === "DropDown" ? (
-            <div className="dropDown-container" key={`hcc-${IdGenerator()}`}>
-              <div className="label">{item.label}</div>
-              <div>
-                <select
-                  name={item.name}
-                  value={data.state}
-                  onChange={(event) => {
-                    
-                    editDataHandler("state", event.target.value);
-                  }}
-                >
-                  {item.values.map((eachOption) => (
-                    <option key={`option-${IdGenerator()}`}>{eachOption}</option>
-                  ))}
-                </select>
+            ) : item.type === "DropDown" ? (
+              <div className="dropDown-container" key={`hcc-${IdGenerator()}`}>
+                <div className="label">{item.label}</div>
+                <div>
+                  <select
+                    name={item.name}
+                    value={data.state}
+                    onChange={(event) => {
+                      editDataHandler("state", event.target.value);
+                    }}
+                  >
+                    {item.values.map((eachOption) => (
+                      <option key={`option-${IdGenerator()}`}>
+                        {eachOption}
+                      </option>
+                    ))}
+                  </select>
+                </div>
               </div>
-            </div>
-          ) : null
-        )}
-        <div>
-          <button onClick={() => onSubmitHandler(data)}>Submit</button>
+            ) : null
+          )}
+          <div>
+            <button onClick={() => onSubmitHandler(data)}>Submit</button>
+          </div>
         </div>
+        {childrenArray.length > 0 && !loading ? (
+          <>
+            <div className="self-dot"></div>
+            <div
+              className="self-dot-vline"
+              style={{
+                height: getVlineHeight(childrenRef, childrenArray.length),
+              }}
+            ></div>
+          </>
+        ) : null}
+        {isNested ? <div className="child-dot"></div> : null}
       </li>
-      <li className="child-li">
-      {childrenArray.length > 0 && childrenArray.map((item) => item)}
-      </li>
+      {childrenArray.length > 0 ? (
+        <li className="child-li" ref={childrenRef}>
+          {childrenArray.map((item) =>
+            React.cloneElement(item, { isNested: true })
+          )}
+        </li>
+      ) : null}
     </ul>
   );
 };
